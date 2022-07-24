c7a815414480a2acd8d55fd5ca8c87e8cda7a2f1
fatima_rosniar <fatimarosniar@gmail.com>
2022-07-24 09:58:00 +0000

    membuat filter data traffic ecom

---
+++ data_clean_ecom.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#1. Gabungkan Data
+csvstack 2019-Nov-sample.csv 2019-Oct-sample.csv > ./2019-OctNov-sample.csv
+
+#2. Penyeleksian Kolom
+csvcut -c 2,3,4,5,6,7,8 2019-OctNov-sample.csv > ./cleandata_kolom.csv
+
+#3. Filtering Data
+csvgrep -c "event_type" -m "purchase" cleandata_kolom.csv > ./cleandata_filter.csv
+
+#4. Splitting Kolom"
+cat cleandata_filter.csv | awk -F ',' '{print $1","$2","$3","$4","$6","$7","$5}'| awk -F '.' '{print $1"."$2","$NF}' > ./data_clean.csv
